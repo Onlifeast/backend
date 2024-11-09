eff3f047069acfabeea58ed0dbb583ac097e16a9
emmanuel tindi <emmanueltindi66@gmail.com>
2024-11-09 20:27:15 +0300

    update orderRoutes

--- src/routes/orderRoutes.ts
+++ src/routes/orderRoutes.ts
@@ -1,26 +1,32 @@
 import express from 'express';
+import { getOrders, addOrder, deleteOrder, updateOrder, getOrderById } from '../controllers/orderController.js';
 
 
 const orderRouter = express.Router();
 
 orderRouter.get('/', (req: express.Request, res: express.Response) => {
     // get all orders (to admin)
+    getOrders(req, res);
 })
 
 orderRouter.get('/:id', (req: express.Request, res: express.Response) => {
     // get order by id (to admin)
+    getOrderById(req, res);
 })
 
 orderRouter.post('/', (req: express.Request, res: express.Response) => {
     // create order (to user)
+    addOrder(req, res);
 })
 
 orderRouter.put('/:id', (req: express.Request, res: express.Response) => {
     // update order (to user)
+    updateOrder(req, res);
 })
 
 orderRouter.delete('/:id', (req: express.Request, res: express.Response) => {
     // delete order (to user)
+    deleteOrder(req, res);
 })
 
 export default orderRouter;
